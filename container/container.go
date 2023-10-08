89a2bac4cbb7f3ab6475d13ef846dae6cb99b235
newbie1111 <harehare753@gmail.com>
2023-10-09 07:17:15 +0900

    add comment of container

--- container/container.go
+++ container/container.go
@@ -1,5 +1,16 @@
 package container
 
+/*
+Container is common interface of data structures.
+
+Container must satisfy the three methods Size, Empty, and Clear.
+
+Size returns number of elements stored in the data structure.
+
+Empty returns wheather the data structure is empty or not.
+
+Clear empties the data structure.
+*/
 type Container interface {
 	Size() int
 	Empty() bool
